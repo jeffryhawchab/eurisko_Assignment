a6e7ef6fa9e8bcedfaf3d0f4ade2e05edb822f89
Jeffry Hawchab <jeffreyhawchab@gmail.com>
2025-03-20 18:37:10 +0200

    challenge 1 have been solved

--- countVowels.ts
+++
@@ -1,6 +0,0 @@
-function ReverseString(str: string): string {
-    return str.split("").reverse().join("");
-}
-
-console.log(ReverseString("Hello World")); // Output: dlroW olleH
-console.log(ReverseString("JavaScript")); // Output: tpircSavaJ
